5f2ea7dcc3a5d47ede400c9f71400bbdb7718ade
Tiago Dimaso <tiagodimaso123@gmail.com>
2025-06-04 14:27:41 -0300

    agregando mas validaciones en socios

--- bibliotecaupe/socios.cpp
+++ bibliotecaupe/socios.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
 #include <string>
 #include <cctype>
+#include <ctime>
 #include "socios.h"
 
-socios::socios( int _dni, int _edad, std::string _apellido, 
-				std::string _genero, std::string _direccion, std::string _numTelefono, 
-				std::string _fechaNacimiento, std::string _email)
-				: dni(_dni), edad(_edad), apellido(_apellido), genero(_genero), 
-				direccion(_direccion), numTelefono(_numTelefono), fechaNacimiento(_fechaNacimiento), email(_email) // A esto se le llama "lista de inicializacion" (lo que esta despues de los : )
-{ }
+socios::socios(int _dni, int _edad, std::string _apellido,
+	std::string _genero, std::string _direccion, std::string _numTelefono,
+	std::string _fechaNacimiento, std::string _email)
+	: dni(_dni), edad(_edad), apellido(_apellido), genero(_genero),
+	direccion(_direccion), numTelefono(_numTelefono), fechaNacimiento(_fechaNacimiento), email(_email) // A esto se le llama "lista de inicializacion" (lo que esta despues de los : )
+{
+}
 
 socios::~socios()
 {
@@ -26,23 +28,7 @@ std::string socios::getGenero() const
 }
 
 std::string socios::getDireccion() const
-std::string socios::getApellido() const
 {
-	return direccion;
-	return apellido;
-}
-
-std::string socios::getTelefono() const
-std::string socios::getGenero() const
-{
-	return numTelefono;
-	return genero;
-}
-
-int socios::getDNI() const
-std::string socios::getDireccion() const
-{
-	return dni;
 	return direccion;
 }
 
@@ -75,26 +61,7 @@ std::string socios::getEmail() const
 
 void socios::setEdad(int nuevaEdad)
 {
-	if (nuevaEdad > 0)
-	{
-	  edad = nuevaEdad;
-	}
-	else
-	{
-		std::cout << "Edad invalida. \n";
-	}
-	
-}
-
-void socios::setApellido(const std::string& nuevoApellido)
-{
-	apellido = nuevoApellido;
-}
-
-
-void socios::setGenero(const std::string& nuevoGenero)
-{
-	genero = nuevoGenero;
+	edad = nuevaEdad;
 }
 
 void socios::setApellido(const std::string& nuevoApellido)
@@ -114,7 +81,6 @@ void socios::setDireccion(const std::string& nuevaDireccion)
 }
 
 void socios::setTelefono(const std::string& nuevoTelefono)
-void socios::setTelefono(const std::string& nuevoTelefono)
 {
 	numTelefono = nuevoTelefono;
 }
@@ -127,12 +93,9 @@ void socios::setFechaNacimiento(const std::string& nuevaFecha)
 
 void socios::setEmail(const std::string& nuevoEmail)
 {
-	numTelefono = nuevoTelefono;
+	email = nuevoEmail;
 }
 
-void socios::setFechaNacimiento(const std::string& nuevaFecha)
-{
-	fechaNacimiento = nuevaFecha;
 // una funcion para mostrar todos los datos
 
 void socios::mostrar() const {
@@ -147,17 +110,72 @@ void socios::mostrar() const {
 }
 
 
-void socios::setEmail(const std::string& nuevoEmail)
-{
-	email = nuevoEmail;
-//funcion para solo validar numeros en numdetelefono
+//funcion para solo validar numeros en numdetelefono. USO FUNCION isdigit de la libreria cctype, me ayuda a verificar si la cadena solo tiene digitos!!!!
 bool telefonoValido(const std::string& telefono) {
 	for (char c : telefono) {
 		if (!isdigit(c)) {
-			return false; // encontramos un caracter que NO es número
+			return false; // encontramos un caracter que NO es numero
 		}
 	}
-	return !telefono.empty(); // tambien valido que no este vacío
+	return !telefono.empty(); // tambien valido que no este vacio
+}
+
+//funcion para que la fecha de nacimiento sea valida del tipo: DD-MM-AAAA
+bool fechaValida(const std::string& fechanacimiento)
+{
+	if (fechanacimiento.length() != 10)
+	{
+		return false;
+	}
+	if (fechanacimiento[2] != '-' || fechanacimiento[5] != '-')
+	{
+		return false;
+	}
+
+	for (int i{ 0 }; i < fechanacimiento.length(); i++)
+	{
+		if (i == 2 || i == 5)
+		{
+			continue;
+		}
+		if (!isdigit(fechanacimiento[i]))
+		{
+			return false;
+		}
+	}
+}
+
+// funcion que aprueba que la fecha sea real. USO LA FUNCION STOI (string to int) AYUDA A LA CONVERSION!!!!
+//TAMBIEN uso funcion substr que marcando las posicion y la cantidad de caracteres extrae una parte del string asi analizo la fecha!!!
+bool fechaReal(const std::string& fecha)
+{
+	int dia{ 0 };
+	int mes{ 0 };
+	int anio{ 0 };
+
+	if (!fechaValida(fecha))
+	{
+		return false;
+	}
+
+	dia = std::stoi(fecha.substr(0, 2));
+	mes = std::stoi(fecha.substr(3, 2));
+	anio = std::stoi(fecha.substr(6, 4));
+
+	if (mes < 1 || mes > 12)
+	{
+		return false;
+	}
+
+	int diasEnMes[] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+	bool bisiesto = (anio % 4 == 0 && anio % 100 != 0) || (anio % 400 == 0);
+	if (bisiesto && mes == 2) diasEnMes[1] = 29;
+
+	if (dia < 1 || dia > diasEnMes[mes - 1]) return false;
+
+	return true;
+
 }
 
 

43bf493d08882292d658ec3b94af876bb08e88f6
agusnzarate <agusnzarate@gmail.com>
2025-06-10 16:23:32 -0300

    Agrega funciones.cpp y funciones.h ; Agrega CSV de lista de libros y socios

---
+++ InterfazGrafica/InterfazGrafica/funciones.h
@@ -0,0 +1,23 @@
+#ifndef FUNCIONES_H
+#define FUNCIONES_H
+
+#include <iostream>
+#include <string>
+
+int pedirDni();
+int pedirEdad();
+std::string pedirDireccion();
+std::string pedirApellido();
+std::string pedirGenero();
+std::string pedirNumTelefono();
+std::string pedirEmail();
+std::string pedirFechaDeNacimiento();        
+
+bool formatoFecha(const std::string& fechanacimiento);
+bool fechaValida(const std::string& fecha);   
+bool emailValido(const std::string& email);
+bool telefonoValido(const std::string& telefono);
+bool direccionValida(const std::string& direccion);
+
+#endif
+

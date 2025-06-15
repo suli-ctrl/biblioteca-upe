c4e4433fff6caac763edf421a519de84a694be0e
Knelones <facundorios230@gmail.com>
2025-06-15 20:58:57 -0300

    Modificación faltante de la Interfaz

--- Builder/BibliotecaUPE.cpp
+++ Builder/BibliotecaUPE.cpp
@@ -4,20 +4,21 @@
 #pragma hdrstop
 #include <tchar.h>
 //---------------------------------------------------------------------------
-USEFORM("RegistrarPrestamo_Form.cpp", Form10);
-USEFORM("RegistrarDevolucion_Form.cpp", Form9);
+USEFORM("RegistrarPrestamo_Form.cpp", RegistrarPrestamoForm);
+USEFORM("RegistrarDevolucion_Form.cpp", RegistarDevoluciónForm);
 USEFORM("Prestamos_Form.cpp", PrestamosForm);
-USEFORM("Socios_Form.cpp", SociosForm);
-USEFORM("VerYBuscarSocio_Form.cpp", Form13);
-USEFORM("VerYBuscarLibro_Form.cpp", VerYBuscarLibroForm);
-USEFORM("VerPrestamo_Form.cpp", Form11);
-USEFORM("Libros_Form.cpp", LibrosForm);
-USEFORM("BajaLibro_Form.cpp", BajaLibroForm);
-USEFORM("AltaSocio_Form.cpp", Form6);
-USEFORM("AltaLibro_Form.cpp", AltaLibroForm);
-USEFORM("BajaSocio_Form.cpp", Form8);
-USEFORM("Inicio_Form.cpp", InicioForm);
 USEFORM("ModificarLibro_Form.cpp", ModificarLibroForm);
+USEFORM("VerYBuscarSocio_Form.cpp", VerYBuscarSocioForm);
+USEFORM("VerYBuscarLibro_Form.cpp", VerYBuscarLibroForm);
+USEFORM("VerPrestamo_Form.cpp", VerPrestamoActivoForm);
+USEFORM("Socios_Form.cpp", SociosForm);
+USEFORM("BajaSocio_Form.cpp", BajaSocioForm);
+USEFORM("BajaLibro_Form.cpp", BajaLibroForm);
+USEFORM("AltaSocio_Form.cpp", AltaSocioForm);
+USEFORM("AltaLibro_Form.cpp", AltaLibroForm);
+USEFORM("Libros_Form.cpp", LibrosForm);
+USEFORM("Inicio_Form.cpp", InicioForm);
+USEFORM("ModificarSocio_Form.cpp", ModificarSocioForm);
 //---------------------------------------------------------------------------
 int WINAPI _tWinMain(HINSTANCE, HINSTANCE, LPTSTR, int)
 {
@@ -30,15 +31,16 @@ int WINAPI _tWinMain(HINSTANCE, HINSTANCE, LPTSTR, int)
 		Application->CreateForm(__classid(TSociosForm), &SociosForm);
 		Application->CreateForm(__classid(TPrestamosForm), &PrestamosForm);
 		Application->CreateForm(__classid(TAltaLibroForm), &AltaLibroForm);
-		Application->CreateForm(__classid(TForm6), &Form6);
+		Application->CreateForm(__classid(TAltaSocioForm), &AltaSocioForm);
 		Application->CreateForm(__classid(TBajaLibroForm), &BajaLibroForm);
-		Application->CreateForm(__classid(TForm8), &Form8);
-		Application->CreateForm(__classid(TForm9), &Form9);
-		Application->CreateForm(__classid(TForm10), &Form10);
-		Application->CreateForm(__classid(TForm11), &Form11);
+		Application->CreateForm(__classid(TBajaSocioForm), &BajaSocioForm);
+		Application->CreateForm(__classid(TRegistarDevoluciónForm), &RegistarDevoluciónForm);
+		Application->CreateForm(__classid(TRegistrarPrestamoForm), &RegistrarPrestamoForm);
+		Application->CreateForm(__classid(TVerPrestamoActivoForm), &VerPrestamoActivoForm);
 		Application->CreateForm(__classid(TVerYBuscarLibroForm), &VerYBuscarLibroForm);
-		Application->CreateForm(__classid(TForm13), &Form13);
+		Application->CreateForm(__classid(TVerYBuscarSocioForm), &VerYBuscarSocioForm);
 		Application->CreateForm(__classid(TModificarLibroForm), &ModificarLibroForm);
+		Application->CreateForm(__classid(TModificarSocioForm), &ModificarSocioForm);
 		Application->Run();
 	}
 	catch (Exception &exception)

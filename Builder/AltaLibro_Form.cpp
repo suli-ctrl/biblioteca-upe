65eacb52a7f84b85545aa6b606e78f65874153e3
agusnzarate <agusnzarate@gmail.com>
2025-06-22 16:44:49 -0300

    Agrega validaciones para libros y socios

--- Builder/AltaLibro_Form.cpp
+++ Builder/AltaLibro_Form.cpp
@@ -24,14 +24,27 @@ void __fastcall TAltaLibroForm::BtnAltaLibroClick(TObject *Sender)
 	String autores = EditAutorLibro->Text;
 	String editorial = EditEditorialLibro->Text;
 	String ubicacion = EditUbiLibro->Text;
-	String estado = ListBoxEstado->Items->Strings[ListBoxEstado->ItemIndex];
 	int anioPublicacion = 0;
 
 
-	try {
-		anioPublicacion = EditAñoLibro->Text.ToInt(); //de string a int
-    } catch (...) {
-		ShowMessage("Año invalido");
+	// Si hay un campo vacio, sale un cartel indicando que se completen los campos
+	if (nombre.IsEmpty() || area.IsEmpty() || subarea.IsEmpty() || autores.IsEmpty() || editorial.IsEmpty() || ubicacion.IsEmpty()) {
+		ShowMessage("Completar todos los campos");
+		return;
+	}
+
+	if (ListBoxEstado->ItemIndex == -1) { //Indica que no hay ningun estado seleccionado (son 4 campos)
+		ShowMessage("Por favor seleccione un estado del libro.");
+		return;
+	}
+	String estado = ListBoxEstado->Items->Strings[ListBoxEstado->ItemIndex];
+
+	if (!TryStrToInt(EditAñoLibro->Text, anioPublicacion)) { //Convierte lo ingresado a INT. Si los caracteres no son numericos, salta el mensaje y return
+		ShowMessage("Año inválido. Ingrese solo números.");
+		return;
+	}
+	if (anioPublicacion < 1500 || anioPublicacion > 2025) {   //Si el valor ingresado no está en este rango, salta el mensaje y return
+		ShowMessage("Ingrese un año de publicación válido (1500 - 2025).");
 		return;
 	}
 
--- Builder/AltaSocio_Form.cpp
+++ Builder/AltaSocio_Form.cpp
@@ -5,6 +5,8 @@
 
 #include "AltaSocio_Form.h"
 #include "biblioteca.h"
+#include <string>
+#include <cctype>
 
 //---------------------------------------------------------------------------
 #pragma package(smart_init)
@@ -20,30 +22,110 @@ void __fastcall TAltaSocioForm::btnDarAltaClick(TObject *Sender)
 {
 	String apellido = txtApellidoAlta->Text;
 	int DNI = 0;
+	int edad = 0;
 	String direccion = txtDireccionCalleAlta->Text;
 	String telefono = txtNumTelAlta->Text;
 	//String fechaNacimiento = txtFechaNacimientoAlta->Text;
 	TDateTime fechaNacimiento = DateTimeSocio->Date;
 
-
+	String dniStr = txtDNIAlta->Text;
+	String edadStr = txtEdadAlta->Text;
 
     String email = txtEmailAlta->Text;
-	int edad = 0;
+
+
+
+
+	//-----------------Validacinoes------------------------------//
+
+
+	//Por si estan vacios
+	if (apellido.IsEmpty() || direccion.IsEmpty() || telefono.IsEmpty() || email.IsEmpty() || edadStr.IsEmpty() || dniStr.IsEmpty() ) {
+
+		ShowMessage("Completar todos los campos.");
+		return;
+	}
+
+	// Apellido
+	for (int i = 1; i <= apellido.Length(); i++) {   //Un for que revisa que cada caracter sea una letra (no acepta numeros ni simbolos)
+		if (!isalpha(apellido[i])) { //isalpha es una funcion de c++ que verifica que el caracter sea una letra
+			ShowMessage("El apellido solo puede tener letras");
+			return;
+		}
+	}
+
+	// DNI
+
+	if (!TryStrToInt(txtDNIAlta->Text, DNI)) {  //Convierte lo ingresado a INT. Si los caracteres no son numericos, salta el mensaje y return
+		ShowMessage("El DNI debe tener solo caracteres numericos");
+		return;
+	}
+	if (DNI < 10000000 || DNI > 99999999) {
+		ShowMessage("El DNI debe tener 8 cifras.");
+		return;
+	}
+
+	// Edad
+
+
+	if (!TryStrToInt(txtEdadAlta->Text, edad)) { //Convierte lo ingresado a INT. Si los caracteres no son numericos, salta el mensaje y return
+		ShowMessage("La edad debe tener solo caracteres numericos");
+		return;
+	}
+	if (edad < 18 || edad > 100) {
+		ShowMessage("La edad debe estar entre 18 y 100 años");
+		return;
+	}
+
+	// Direccion        //FindFirstNotOf no funca
+	/*
+	if (direccion.FindFirstNotOf(" ") == -1) { // uso find first not of que busca en el string un caracter distinto al que le paso, esto me sirve para ver si el usuario ingresa todo espacios, como le mando espacios si solo encuantra eso devuelve npos
+		ShowMessage("Ingrese una direccion valida.");
+		return;
+	}
+	*/
+	for (int i = 1; i <= direccion.Length(); i++) { //Recorro caracter a caracter la direccion
+		if (!(isalnum(direccion[i]) || direccion[i] == ' ')) { //Busco caracteres que no sean letra, numero o espacio (en caso de ser asi salta el mensaje y return)
+			ShowMessage("La direccion solo puede contener letras, numeros y espacios.");
+			return;
+		}
+	}
+
+	// Telefono
+	if (telefono.Length() != 10) {
+		ShowMessage("El telefono debe tener 10 digitos");
+		return;
+	}
+	for (int i = 1; i <= telefono.Length(); i++) {
+		if (!isdigit(telefono[i])) { //Si no es digito (numeros) es invalido y salta el mensaje y return
+			ShowMessage("El telefono solo debe contener numeros");
+			return;
+		}
+	}
+
+	// Email
+	int arrobas = 0, puntos = 0;
+	for (int i = 1; i <= email.Length(); i++) {
+		if (email[i] == '@') arrobas++;
+		else if (email[i] == '.') puntos++;
+		else if (!isalnum(email[i])) {
+			ShowMessage("El email contiene caracteres inválidos.");
+			return;
+		}
+	}
+	if (arrobas != 1 || puntos != 1 || email.Pos("@") > email.Pos(".")) {
+		ShowMessage("El email debe tener formato válido: usuario@dominio.com");
+		return;
+	}
+
+	// Genero
+	if (lstGeneroAlta->ItemIndex == -1) {
+		ShowMessage("Seleccione un genero");
+		return;
+	}
 	String genero = lstGeneroAlta->Items->Strings[lstGeneroAlta->ItemIndex];
 
-	try {
-		DNI = txtDNIAlta->Text.ToInt(); //de string a int
-	} catch (...) {
-		ShowMessage("DNI invalido");
-		return;
-	}
 
-	try {
-		edad = txtEdadAlta->Text.ToInt(); //de string a int
-	} catch (...) {
-		ShowMessage("edad invalida");
-		return;
-	}
 
 	bibliotecaUPE.altaSocio(DNI, edad, apellido, genero, direccion, telefono, fechaNacimiento, email);
 
